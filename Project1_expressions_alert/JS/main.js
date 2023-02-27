5df06f635dcbf8c63ebea44ade8d5f0c142b5845
eden <99998327+edenosman@users.noreply.github.com>
2023-02-27 17:37:57 +0000

    Project 1 - Expression Alert

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+// Defining some variables and giving them values
+var Course = "Web Development", HypertextLanguage = "HTML", StyleSheetLanguage = "CSS", ProgrammingLanguage = "JavaScript";
+// The next line cause an alert window to pop up
+window.alert("Hello, I'm learning JavaScript.");
+// Defining two variables and giving them string values
+var A = "Nothing is "
+var B = "impossible"
+
+// In the next line, I used fontcolor method to change the color of variable B to green.
+var B = B.fontcolor("green")
+// On the next line I concatenated variables A and B.
+var C = A + B;
+// document.write method is used to display text, in this case variable C.
+document.write(C);
