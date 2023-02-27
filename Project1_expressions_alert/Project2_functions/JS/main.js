5b39b41ffc2baf383c406a344f47524d59a500b8
eden <99998327+edenosman@users.noreply.github.com>
2023-02-27 17:48:11 +0000

    Delete Project1_expressions_alert/Project2_functions directory

--- Project1_expressions_alert/Project2_functions/JS/main.js
+++
@@ -1,9 +0,0 @@
-// Defined a function and named it "myFunction"
-function myFunction() {
-    // I defined a variable and I gave it a string value: "I am learning"
-    var sentence = "I am learning";
-    // I concatenated the variable "sentence"
-    sentence += " a lot from this course!";
-    // Putting the value of result into HTML element with "Concatenate" id
-    document.getElementById("Concatenate").innerHTML = sentence;
-}
